0a52fd3e91ef1e6134d9491c83cfaa94e3b3b31e
Olga <olga_grabchak@i.ua>
2017-11-09 22:54:48 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,45 @@
+// const fib = parseInt(prompt('F(n)'));
+// let result;
+// switch (fib) {
+//     case 1:
+//         result = '1';
+//         break;
+//     case 2:
+//         result = '1';
+//         break;
+//     case 3:
+//         result = '2';
+//         break;
+//     case 4:
+//         result = '3';
+//         break;    
+//     case 5:
+//         result = '5';
+//         break;
+//     case 6:
+//         result = '8';
+//         break;
+//     case 7:
+//         result = '13';
+//         break;    
+//     case 8:
+//         result = '21';
+//         break;
+//     case 9:
+//         result = '34';
+//         break;
+//     case 10:
+//         result = '55';
+//         break;    
+    
+// }
+// document.write(result);
+
+
+for (let i = 1; i < 56; i++) {
+    if (i % 2){
+        break;
+    }
+    document.write(i);
+}
+
