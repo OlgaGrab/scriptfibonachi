3e4a9609f482337438414c83ab77b9161cde02e4
Olga <olga_grabchak@i.ua>
2017-11-10 18:24:58 +0000

    Innitial

--- js/index.js
+++ js/index.js
@@ -1,54 +1,54 @@
-// const fib = parseInt(prompt('F(n)'));
-// let result;
-// switch (fib) {
-//     case 1:
-//         result = '1';
-//         break;
-//     case 2:
-//         result = '1';
-//         break;
-//     case 3:
-//         result = '2';
-//         break;
-//     case 4:
-//         result = '3';
-//         break;    
-//     case 5:
-//         result = '5';
-//         break;
-//     case 6:
-//         result = '8';
-//         break;
-//     case 7:
-//         result = '13';
-//         break;    
-//     case 8:
-//         result = '21';
-//         break;
-//     case 9:
-//         result = '34';
-//         break;
-//     case 10:
-//         result = '55';
-//         break;    
+const fib = parseInt(prompt('F(n)'));
+let result;
+switch (fib) {
+    case 1:
+        result = '1';
+        break;
+    case 2:
+        result = '1';
+        break;
+    case 3:
+        result = '2';
+        break;
+    case 4:
+        result = '3';
+        break;    
+    case 5:
+        result = '5';
+        break;
+    case 6:
+        result = '8';
+        break;
+    case 7:
+        result = '13';
+        break;    
+    case 8:
+        result = '21';
+        break;
+    case 9:
+        result = '34';
+        break;
+    case 10:
+        result = '55';
+        break;    
     
-// }
-// document.write(result);
-
-
-
-const number = parseInt(prompt('Number of progression'));
-     function fib(n) {
-        let = n;
-        if ((n => 1) && (n <= 10)) {
-        return fib(n-1) + fib(n-2);
-    }
-        else if ((n < 1) && (n > 10)) {
-        return 'null' ; }
-        else if (n = 1) {
-            return n;
-        }
 }
-    const result = fib(n)
+document.write(result);
+
+
+
+// const number = parseInt(prompt('Number of progression'));
+//      function fib(n) {
+//         let = n;
+//         if ((n => 1) && (n <= 10)) {
+//         return fib(n-1) + fib(n-2);
+//     }
+//         else if ((n < 1) && (n > 10)) {
+//         return 'null' ; }
+//         else if (n = 1) {
+//             return n;
+//         }
+// }
+//     const result = fib(n)
        
-    document.write(result);
+//     document.write(result);
